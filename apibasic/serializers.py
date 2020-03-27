a026da79b2b271141c21ce299639142e74d32cb3
sakhawatliv <sakhawatliv@gmail.com>
2020-03-27 19:37:30 +0600

    inital commit

---
+++ apibasic/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Article
+
+
+# class ArticleSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=100)
+#     author = serializers.CharField(max_length=100)
+#     email = serializers.EmailField(max_length=100)
+#     date = serializers.DateTimeField()
+#
+#     def create(self, validated_data):
+#         return Article.objects.create(validated_data)
+#
+#
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title',instance.title)
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id','title','author']
